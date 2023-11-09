020477aca360207fcd58dea16d90dcecdb27e23f
Aadil Hussain <ault@andrims.com>
2023-11-09 17:33:18 -0500

    added exception

--- assign_to_student_test.py
+++ assign_to_student_test.py
@@ -5,9 +5,9 @@ def test_assign_to_student():
         # these two assertions are the actual tests
         assert assign_to_student.submission(2) == 4 
         assert assign_to_student.submission(-1) == -2
-        print("All Tests Passed") # print statements should appear as github comments
+        print("All Tests Passed") # print statements should appear in "actions" tab
     except:
-        print("Tests Failed")
+        raise Exception("Tests Failed")
 
     # if you want to print specifically what testcase failed,
     # then you can create separate try/except clauses
