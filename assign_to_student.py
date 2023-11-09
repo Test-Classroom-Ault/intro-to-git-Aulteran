dc1f5a23d7159b9870c75b7704f843825c54b091
Aadil Hussain <ault@andrims.com>
2023-11-09 17:46:32 -0500

    added args

--- assign_to_student.py
+++ assign_to_student.py
@@ -5,9 +5,12 @@ use the parameter 'number' as an input
 return the answer as variable 'output'
 code your response in the function "submission"
 '''
+import sys
 
 def submission(number):
   output = int()
   # code your submission here
   output = number*2  
   return output
+
+submission(sys.argv[0])
